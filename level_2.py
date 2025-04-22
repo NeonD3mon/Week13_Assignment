602f259545921d03a6d16836e5351c0af1fe4958
Itamar Afek <ItamarAfek@Itamars-Mac.local>
2025-04-22 18:36:17 -0400

    Level 2 complete

---
+++ level_2.py
@@ -0,0 +1,6 @@
+mix_of_stuff = [True, 1, "Apple", 3.14]
+
+playlist_for_coding = ["Free Your Dreams by Snarky Puppy", "Freedom Jazz Dance by Eddie Harris", "In My Mind by Ariza", "What's Up Danger by Blackway", "Hourglass by Disclosure"]
+print(playlist_for_coding[2])
+
+playlist_for_coding.append("Never Gonna Give You Up by Rick Astley")
