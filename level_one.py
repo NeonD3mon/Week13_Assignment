f92c3509562121319f1f807e969d762ded0495e1
Itamar Afek <ItamarAfek@Itamars-Mac.local>
2025-04-22 18:25:27 -0400

    level 1 complete

---
+++ level_one.py
@@ -0,0 +1,5 @@
+print("\"What do you call a fish with no eyes?\" I paused, puzzled, until he grinned and said, \"Fsh!\"")
+
+print("up \ndown")
+
+print("Line 1 \n Line 2 \n  Line 3 \n Line 4 \nLine5")
